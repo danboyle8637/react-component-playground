82a96e6584ff551f3227c3cbba2a2aeae2095906
Dan <danboyle8637@gmail.com>
2022-07-02 17:41:58 +0000

    Update BaseLayout.tsx

--- src/Layouts/BaseLayout.tsx
+++ src/Layouts/BaseLayout.tsx
@@ -1,29 +1,19 @@
-import { useRouter } from 'next/router'
-import styled from 'styled-components'
+import { useRouter } from "next/router";
+import { ReactNode } from "react";
+import styled from "styled-components";
 
-import Global from '../styles/Global'
+import Global from "../styles/Global";
 
-const Layout: React.FC = ({ children }) => {
-  const { pathname } = useRouter()
-
-  return (
-    <>
-      <BaseContainer>
-        <Global />
-        <ContentContainer>{children}</ContentContainer>
-      </BaseContainer>
-    </>
-  )
+interface LayoutProps {
+  children: ReactNode;
 }
-
-export default Layout
 
 const BaseContainer = styled.main`
   display: flex;
   flex-direction: column;
   align-items: center;
   width: 100%;
-`
+`;
 
 const ContentContainer = styled.div`
   display: flex;
@@ -32,4 +22,19 @@ const ContentContainer = styled.div`
   width: 100%;
   max-width: 1440px;
   overflow: hidden;
-`
+`;
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  // const { pathname } = useRouter()
+
+  return (
+    <>
+      <BaseContainer>
+        <Global />
+        <ContentContainer>{children}</ContentContainer>
+      </BaseContainer>
+    </>
+  );
+};
+
+export default Layout;
